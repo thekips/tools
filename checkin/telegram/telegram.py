b2d031d20eb013ba676f7d89bef4d3dce297b911
thekips <issizhe@gmail.com>
2023-01-16 17:55:44 +0800

    upgrade telegram

--- checkin/telegram/telegram.py
+++ checkin/telegram/telegram.py
@@ -4,17 +4,35 @@ import sys
 import time
 from telethon import TelegramClient, events, sync
 
-api_id = [26449713]	#输入api_id，一个账号一项
-api_hash = [sys.argv[1]]	#输入api_hash，一个账号一项
+api_id = 26449713
+api_hash = sys.argv[0]
 
-session_name = api_id[:]
-for num in range(len(api_id)):
-	session_name[num] = "id_" + str(session_name[num])
-	client = TelegramClient(session_name[num], api_id[num], api_hash[num])
-	client.start()
-	client.send_message("@fooandfriends_bot", '/checkin')	#第一项是机器人ID，第二项是发送的文字
-	time.sleep(5)	#延时5秒，等待机器人回应（一般是秒回应，但也有发生阻塞的可能）
-	client.send_read_acknowledge("@fooandfriends_bot")	#将机器人回应设为已读
-	print("Done! Session name:", session_name[num])
-	
+session_name = "id_" + str(api_id)
+# print(session_name, api_id, api_hash)
+
+client = TelegramClient(session_name, api_id, api_hash)
+client.start()
+
+# Send to fnf_bot
+client.send_message("@fooandfriends_bot", '/checkin')	#第一项是机器人ID，第二项是发送的文字
+time.sleep(2)	#延时2秒，等待机器人回应（一般是秒回应，但也有发生阻塞的可能）
+client.send_read_acknowledge("@fooandfriends_bot")	#将机器人回应设为已读
+info = client.get_messages("@fooandfriends_bot") #获取消息
+if len(info) >= 0:
+    print(info[0].message)
+else:
+    print('ERR, NO MESSAGE..')
+
+# Send to yellow_gif_bot
+for i in range(3):
+    info = client.get_messages("@yellow_gif_bot", 2) #获取消息
+    
+    if len(info) >= 0:
+        print(info[0].message)
+    else:
+        print('ERR, NO MESSAGE..')
+        
+    client.send_message('@yellow_gif_bot', message=info[1])
+    time.sleep(2)
+
 os._exit(0)
