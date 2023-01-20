9cf5e7c000776dd5e7da202d5d45aeceb0cafd64
Thekips <issizhe@gmail.com>
2023-01-20 17:21:10 +0800

    Update feiyue.py

--- checkin/feiyue.py
+++ checkin/feiyue.py
@@ -6,33 +6,31 @@ import random
 from checkin_logger import logger
 from parsel import Selector
 
-cookies = sys.argv[1]
   
 EXP_URL = 'https://bbs.kfpromax.com/kf_growup.php?ok=3&safeid=%s'
 GAME_URL = 'https://bbs.kfpromax.com/kf_fw_ig_index.php'
 BATTLE_URL = 'https://bbs.kfpromax.com/kf_fw_ig_intel.php'
 
+user = 'xlrt'
+passwd = sys.argv[1]
+
+# Create session
 headers = {
-    'authority': 'bbs.kfpromax.com',
-    'accept': '*/*',
-    'accept-language': 'en,zh-CN;q=0.9,zh;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'cookie': cookies, 
-    'origin': 'https://bbs.kfpromax.com',
-    'referer': 'https://bbs.kfpromax.com/kf_fw_ig_index.php',
-    'sec-ch-ua': '"Not_A Brand";v="99", "Microsoft Edge";v="109", "Chromium";v="109"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.55',
-    'x-requested-with': 'XMLHttpRequest',
 }
 session = requests.session()
 session.headers = headers
 # logger.info(headers)
 
+
+# Login
+data = 'forward=&step=2&lgt=1&hideid=0&cktime=31536000&pwuser={}&pwpwd={}'.format(user, passwd)
+response = session.post('https://bbs.kfpromax.com/login.php', headers=headers, data=data)
+
+
+# Verify if login is success
 response = session.get(GAME_URL)
 if response.status_code == 200:
     safeid = re.findall(r'(?<=safeid=).*(?=\")', response.text)
@@ -40,11 +38,12 @@ if response.status_code == 200:
         safeid = safeid[0]
         logger.info('safeid is: %s' % safeid)
     else:
-        logger.error('Process end, Can\'t find safeid...')
+        logger.error('Login failure, Can\'t find safeid...')
         sys.exit()
 else:
     logger.error(response.status_code)
     sys.exit()
+
 
 # Checkin
 data = {
@@ -54,11 +53,14 @@ data = {
 response = session.get(EXP_URL % safeid)
 logger.info('Check End...')
 
-# Game
+
+# Game Start
 data = {
     'safeid': safeid,
 }
+response = session.get(GAME_URL)
 while True:
+    time.sleep(random.random() + 1)
     response = session.post(BATTLE_URL, data=data)
 
     if response.text == 'no':
@@ -68,4 +70,3 @@ while True:
         selector = Selector(response.text)
         info = selector.xpath('//text()').getall()
         logger.info(info)
-        time.sleep(random.random() + 1)
